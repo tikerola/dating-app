0ef9bae1032d2c6067d5e12f02136b27181b7fbe
timo kerola <timo.kerola@edu.bc.fi>
2019-11-27 16:22:36 +0200

    lets put it to heroku

--- back-end/app.js
+++ back-end/app.js
@@ -12,7 +12,7 @@ const tokenFromHeaders = require('./middlewares/tokenFromHeaders')
 const cors = require('cors')
 
 
-
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
