9d29525db3013aa49892a301d59f647c5f112b76
timo kerola <timo.kerola@edu.bc.fi>
2019-11-29 09:40:33 +0200

    pagination for favorites added

--- front-end/src/components/ContentArea/Favorites.js
+++ front-end/src/components/ContentArea/Favorites.js
@@ -41,42 +41,47 @@ const useStyles = makeStyles({
 })
 
 const Favorites = ({ profiles }) => {
-  
+  const [page, setPage] = React.useState(1)
 
   const classes = useStyles()
 
   const handlePageChange = direction => {
+    if (direction === 'next')
+      setPage(page + 1)
 
-    
+    else if (direction === 'prev')
+      setPage(page - 1)
+
   }
 
-  
+
   return (
     <div className={classes.root}>
       <h1>{profiles.length > 0 ? 'Your Favorites' : 'No favorites chosen yet'}</h1>
       <div className={classes.center}>
         <div className={classes.container}>
           {
-            profiles.map(profile => <Link to={`/search/profiles/${profile.username}`} key={profile.id}>
-              <ProfileThumbnail
-                username={profile.username}
-                image={profile.image.imageUrl}
-                online={profile.online}
-              />
+            profiles.slice((page - 1) * 12, 12 + (page - 1) * 12).map(profile =>
+              <Link to={`/search/profiles/${profile.username}`} key={profile.id}>
+                <ProfileThumbnail
+                  username={profile.username}
+                  image={profile.image.imageUrl}
+                  online={profile.online}
+                />
               </Link>)
           }
         </div>
       </div>
       {profiles.length > 0 && <div className={classes.pagination}>
         <Button
-          disabled={true}
+          disabled={page === 1}
           onClick={() => handlePageChange('prev')}
         >
           {'< '}prev page
         </Button>
 
         <Button
-          disabled={true}
+          disabled={profiles.length <= page * 12}
           onClick={() => handlePageChange('next')}
         >
           next page{' >'}
