63262896914db32ac87ff51c766dcf04b27572dd
timo kerola <timo.kerola@edu.bc.fi>
2019-12-03 12:46:28 +0200

    profiles reducer testing...

---
+++ front-end/src/tests/reducers/profiles.js
@@ -0,0 +1,34 @@
+import profilesReducer from '../../reducers/profiles'
+import deepFreeze from 'deep-freeze'
+
+describe('profiles reducer', () => {
+
+  test('SEARCH_PROFILES', () => {
+    const state = []
+
+    const action = {
+      type: 'SEARCH_PROFILES',
+      profiles: [{
+        id: 1,
+        username: 'timo',
+        loggedIn: true
+      },
+      {
+        id: 2,
+        username: 'hanna',
+        loggedIn: false
+      }]
+    }
+
+    deepFreeze(state)
+    const newState = profilesReducer(state, action) 
+
+    expect(newState).toEqual(action.profiles)
+
+  })
+
+  test('should reset state to initial state', () => {
+    
+  })
+
+})
